ae42cec153826645433f497b3e648560d5fa5599
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2018-05-21 17:48:42 +0300

    Added MyCarsActivity.

---
+++ app/src/main/java/ua/com/vbychkovskyi/carlog/activity/MyCarsActivity.kt
@@ -0,0 +1,14 @@
+package ua.com.vbychkovskyi.carlog.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ua.com.vbychkovskyi.carlog.R
+
+
+class MyCarsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_cars)
+    }
+}
