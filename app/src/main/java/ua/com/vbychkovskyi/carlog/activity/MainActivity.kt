8a9d46ec1bd73155f5914a062f1b385fd0460868
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2018-05-22 15:11:06 +0300

    Integration with google sign in.

--- app/src/main/java/ua/com/vbychkovskyi/carlog/activity/MainActivity.kt
+++ app/src/main/java/ua/com/vbychkovskyi/carlog/activity/MainActivity.kt
@@ -3,32 +3,41 @@ package ua.com.vbychkovskyi.carlog.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import ua.com.vbychkovskyi.carlog.R
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.common.api.ApiException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        findViewById<SignInButton>(R.id.sign_in_button)
+                .setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id ?: "" != R.id.sign_in_button) {
+            return
+        }
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build()
 
         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        findViewById<SignInButton>(R.id.sign_in_button)
-                .setOnClickListener {
-                    val signInIntent = mGoogleSignInClient.signInIntent
-                    startActivityForResult(signInIntent, 10)
-                }
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 10)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
