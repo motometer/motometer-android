b22f5b76742dbd867afd8bbcb8f4261bb709325a
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2018-05-22 16:49:18 +0300

    Integration with google sign in.
    Added sign out button.

--- app/src/main/java/ua/com/vbychkovskyi/carlog/activity/MainActivity.kt
+++ app/src/main/java/ua/com/vbychkovskyi/carlog/activity/MainActivity.kt
@@ -3,63 +3,31 @@ package ua.com.vbychkovskyi.carlog.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import ua.com.vbychkovskyi.carlog.R
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.common.api.ApiException
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.sign_out)
+                .setOnClickListener {
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .build()
 
+                    val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
+                    mGoogleSignInClient.signOut()
+                }
 
-        findViewById<SignInButton>(R.id.sign_in_button)
-                .setOnClickListener(this)
-    }
+        val account = GoogleSignIn.getLastSignedInAccount(this)
 
-    override fun onClick(v: View?) {
-        if (v?.id ?: "" != R.id.sign_in_button) {
-            return
-        }
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build()
-
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        val signInIntent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 10)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 10) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-
-            // Signed in successfully, show authenticated UI.
-            println(account)
-        } catch (e: ApiException) {
-            println(e)
+        if (account == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/ua/com/vbychkovskyi/carlog/activity/SignInActivity.kt
@@ -0,0 +1,64 @@
+package ua.com.vbychkovskyi.carlog.activity
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import ua.com.vbychkovskyi.carlog.R
+
+
+class SignInActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        findViewById<SignInButton>(R.id.sign_in_button)
+                .setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id ?: "" != R.id.sign_in_button) {
+            return
+        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 10)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 10) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // Signed in successfully, show authenticated UI.
+            println(account)
+        } catch (e: ApiException) {
+            println(e)
+        }
+    }
+}
