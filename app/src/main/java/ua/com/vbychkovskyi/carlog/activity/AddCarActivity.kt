7240815174211ea052c136540ff398194f1f01d3
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2018-05-31 19:53:23 +0300

    Added create car button listener.

--- app/src/main/java/ua/com/vbychkovskyi/carlog/activity/AddCarActivity.kt
+++ app/src/main/java/ua/com/vbychkovskyi/carlog/activity/AddCarActivity.kt
@@ -1,7 +1,10 @@
 package ua.com.vbychkovskyi.carlog.activity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 import ua.com.vbychkovskyi.carlog.R
 
 class AddCarActivity : AppCompatActivity() {
@@ -9,5 +12,12 @@ class AddCarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_car)
+        findViewById<Button>(R.id.create_car).setOnClickListener({
+            val carName = findViewById<TextView>(R.id.car_name).text
+            val carMadeYear = findViewById<TextView>(R.id.car_made_year_input).text
+            val carRunDistance = findViewById<TextView>(R.id.car_run_distance).text
+
+            startActivity(Intent(this, MainActivity::class.java))
+        })
     }
 }
